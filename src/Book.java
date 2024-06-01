bff2e372ce3e1102b3d900f5e22de1f85e318216
pooja bharti <poojabharti80@gmail.com>
2024-06-01 10:58:05 +0300

    line added

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Book {
+public class Book {
+    String title;
+    String author;
+    String ISBN;
+
+    //Constructor with default value
+    public Book(){
+        this.title = "unknown";
+        this.author= "unknown";
+        this.ISBN = "000000";
+    }
+
+    //Extend book class by adding constructor with title,author and isbn
+    public Book(String title,String author,String ISBN){
+        this.title=title;
+        this.author=author;
+        this.ISBN=ISBN;
+    }
+    public Book(String title, String author){
+        this.title= title;
+        this.author=author;
+        this.ISBN="123456";
+    }
 }
--- src/Booklibrary.java
+++ src/Booklibrary.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Booklibrary {
+public class Booklibrary {
+    public static void main(String[]args){
+        Book book1st = new Book();
+        Book book2nd = new Book("Harry Potter","JK","9780747532743");
+        Book book3rd = new Book("Ramayana","Tulsidas");
+        System.out.println();
+        System.out.println(book1st.title +","+ book1st.author+","+book1st.ISBN);
+        System.out.println(book2nd.title+","+book2nd.author+","+book2nd.ISBN);
+        System.out.println(book3rd.title+","+book3rd.author+","+book3rd.ISBN);
+    }
 }
